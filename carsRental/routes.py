b5af9fd97d22c26cf80a17defb33f16a152e4496
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-02-12 17:34:00 +0200

    Added registration page

--- carsRental/routes.py
+++ carsRental/routes.py
@@ -1,6 +1,6 @@
 from carsRental import app
 from flask import render_template, url_for
-from carsRental.forms import RegistrationForm
+from carsRental.forms import LoginForm, RegistrationForm
 
 @app.route("/")
 def home():
@@ -9,4 +9,9 @@ def home():
 @app.route("/register")
 def register():
     form = RegistrationForm()
+    return render_template('register.html', title = "Register", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
     return render_template('register.html', title = "Register", form=form)
