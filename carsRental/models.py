3e2df8e683b74fb51e6dfb0024747feea45aefdc
marti456 <martidami5@gmail.com>
2021-03-04 02:32:04 +0200

    Can add cars to database and repairs in login with admin account

--- carsRental/models.py
+++ carsRental/models.py
@@ -10,22 +10,36 @@ from flask_login import UserMixin
 def load_user(user_id):
     return Admin.query.get(user_id)
 
+class Car(db.Model, UserMixin): # to improve the car db model
+    id = db.Column(db.Integer, primary_key=True)
+    #filename = db.Column(db.String(128), nullable=False)
+    model = db.Column(db.String(20), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #content = db.Column(db.Text, nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('admin.id'), nullable=False)
+    image = db.Column(db.LargeBinary, nullable=False)
+    rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
+
+    def __repr__(self):
+        return f"Car('{self.model}', {self.date_posted})"
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    #cars =  db.relationship('Car', backref='uploader', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', {self.email})"
+
 class Admin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    cars =  db.relationship('Car', backref='uploader', lazy=True)
 
     def __repr__(self):
         return f"Admin('{self.username}', {self.email})"
 
-class Car(db.Model): # to improve the car db model
-    id = db.Column(db.Integer, primary_key=True)
-    model = db.Column(db.String(20), unique=True, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('admin.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Car('{self.model}', {self.date_posted})"
+db.create_all()
--- carsRental/routes.py
+++ carsRental/routes.py
@@ -1,12 +1,16 @@
 from flask_login.utils import logout_user
 from werkzeug.utils import redirect
 from wtforms.validators import Email
-from carsRental.models import Admin
+from carsRental.models import *
 from carsRental import app, bcrypt, db
-from flask import render_template, url_for, flash, redirect
+from flask import request, render_template, url_for, flash, redirect
 from carsRental.forms import LoginForm, RegistrationForm
 from carsRental.models import Admin 
 from flask_login import login_user, current_user
+from base64 import b64encode
+import base64
+from io import BytesIO #Converts data from Database into bytes
+
 
 
 @app.route("/")
@@ -25,18 +29,30 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
+        username = User.query.filter_by(username=form.username.data).first()
+        if username:
+            flash('That username is taken. Choose another one.', 'danger')
+            return redirect(url_for('register'))
+
         admin_username = Admin.query.filter_by(username=form.username.data).first()
         if admin_username:
             flash('That username is taken. Choose another one.', 'danger')
             return redirect(url_for('register'))
+
+
+        email = User.query.filter_by(email=form.email.data).first()
+        if email:
+            flash('That email is taken. Choose another one.', 'danger')
+            return redirect(url_for('register'))
+
         admin_email = Admin.query.filter_by(email=form.email.data).first()
         if admin_email:
             flash('That email is taken. Choose another one.', 'danger')
             return redirect(url_for('register'))
         # valid username and email
         hashed_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        admin = Admin(username=form.username.data, email=form.email.data, password=hashed_pass)
-        db.session.add(admin)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pass)
+        db.session.add(user)
         db.session.commit()
         flash(f'Created account for {form.username.data}. You can now log in.', 'success')
         return redirect(url_for('login'))
@@ -48,11 +64,49 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if(user and bcrypt.check_password_hash(user.password, form.password.data)):
+            login_user(user, remember=form.remember.data)
+            flash('You have been logged in!', 'success')
+            return render_template('home.html', title = "Home")
+
         admin = Admin.query.filter_by(username=form.username.data).first()
         if(admin and bcrypt.check_password_hash(admin.password, form.password.data)):
             login_user(admin, remember=form.remember.data)
             flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login unsuccessful. Please check username and password', 'danger')
+            return render_template('home.html', title = "Home", is_admin = True)
+        
+        flash('Login unsuccessful. Please check username and password', 'danger')
+
     return render_template('login.html', title = "Login", form=form)
+
+
+#@app.route('/add_car', methods=['GET', 'POST'])
+#    return render_template('add_car.html')
+
+def render_picture(data):
+
+    render_pic = base64.b64encode(data).decode('ascii') 
+    return render_pic
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('add_car.html')
+    else:
+        db.create_all()
+        file = request.files['inputFile']
+        data = file.read()
+        render_file = render_picture(data)
+        text = request.form['text']
+        location = request.form['location']
+
+        newCar = Car(image=data, rendered_data=render_file, model=text)
+        db.session.add(newCar)
+        db.session.commit() 
+        flash(f'Pic {newCar.name} uploaded Text: {newCar.text} Location: {newCar.location}')
+
+        return render_template('upload.html')
+
+
+
